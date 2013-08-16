262c2a8c2dd3220d8cbe99d50edf9f16f45ae9cb
Erik Ordway <ordwaye@evergreen.edu>
2013-08-15 17:15:32 -0700

    fix config

--- lib/entran.rb
+++ lib/entran.rb
@@ -19,11 +19,9 @@ require 'net/http/post/multipart'
 # you just need to require this one file in your bin file
 
 def load_files(files, kind, presence, ims_key, banner_host)
-  @ims_xml = Nokogiri::XML(open("https://#{banner_host}/banner/public/program/feed?feed_type=moodle&key=#{ims_key}&min_term=201210",:ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE))
+  @ims_xml = Nokogiri::XML(open("https://#{banner_host}/banner/public/program/feed?feed_type=moodle&key=#{ims_key}&min_term=201210", :ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE))
   @lms_courses_xml = Nokogiri::XML(open("http://#{presence}/feeds/lms_courses.xml"))
   @terms_xml = Nokogiri::XML(open("http://#{presence}/feeds/#{kind}/terms.xml"))
-  #@catalog-2013_xml = Nokogiri::XML(open("http://#{banner_host}/banner/public/program/catalog/2013"))
-  #@catalogs = Hash[2013 =>  Nokogiri::XML(open('http://#{banner_host}/banner/public/program/catalog/2013')) , 2014 =>  Nokogiri::XML(open('http://adminweb.evergreen.edu/banner/public/program/catalog/2014'))]
   @terms_csv = open("http://#{presence}/feeds/#{kind}/terms.csv")
 
   puts @catalogs.class
@@ -53,7 +51,7 @@ def course_created?(sis_id, global_options)
     puts "----------------->trying to find out if course is on canvas host"
     response = http.request(req)
 
-    return response.code.to_i == 200      #if 200 then then course has been created so we can skip it.
+    return response.code.to_i == 200 #if 200 then then course has been created so we can skip it.
 
   end
   return false
